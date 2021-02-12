3879faec0c4ad4e82f8328cfb32b5357329c64a4
CSCMe <19313511+CSCMe@users.noreply.github.com>
2021-02-12 02:10:40 +0100

    Updates Ship List

--- src/main/java/ee/tkasekamp/europawaranalyzer/util/Constants.java
+++ src/main/java/ee/tkasekamp/europawaranalyzer/util/Constants.java
@@ -3,5 +3,5 @@ package ee.tkasekamp.europawaranalyzer.util;
 public class Constants {
 	/* Reference list to determine the battleType */
 	public final static String[] NAVAL_UNITS = {"clipper_transport", "commerce_raider", "cruiser", "dreadnougth", "frigate", "ironclad",
-			"manowar", "monitor", "steam_transport", "battleship"};
+			"manowar", "monitor", "steam_transport", "battleship", "light_ship", "heavy_ship", "transport", "galley"};
 }
